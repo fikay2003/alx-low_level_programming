17ae193514d66edb81a992aae069624b62f62f57
Ayodele <fortunatefikayomi@gmail.com>
2022-09-08 09:33:43 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
- * what
- * 
- * about this
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); 
+	puts("\"Programiing is like building a multilingual puzzle"); 
 	return (0);
 }
