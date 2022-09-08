b65982bc1d316b5f54b0b237b8c9d71d6c549d63
Ayodele <fortunatefikayomi@gmail.com>
2022-09-08 09:09:11 -0700

    my first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+
+/*
+ * what
+ * 
+ * about this
+ */
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle"; 
-	puts(Mystr);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle"); 
+	return (0);
 }
