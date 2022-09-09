cfff06cbda338a828449c4a564d2556e3cbbc433
Ayodele <fortunatefikayomi@gmail.com>
2022-09-08 22:07:58 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,9 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-/*
- *Trim trailing space
- */
-	str. trim();
-puts("\"Programming is like building a multilingual puzzle"); 
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
