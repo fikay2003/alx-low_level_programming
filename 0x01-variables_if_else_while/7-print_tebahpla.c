9d9f66fcaa7aef930263ef6d0819d3d0bb6f9188
Ayodele <fortunatefikayomi@gmail.com>
2022-09-11 21:48:31 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: always 0
+*/
+int main(void)
+{
+char c = 'z';
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
