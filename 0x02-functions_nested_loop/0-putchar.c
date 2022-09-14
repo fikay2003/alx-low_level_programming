de3596b68343162866838dcf77f5e13a59ee9fd8
Ayodele <fortunatefikayomi@gmail.com>
2022-09-14 07:49:01 -0700

    putchar

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+* main - entry point
+*
+* Return: always 0
+*/
+void myfunction()
+{
+putchar('_putchar');
+}
+int main(void) 
+{
+myFunction();
+putchar('\n');
+return (0);
+}
