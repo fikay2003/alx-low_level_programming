ad5aa365fbf3a0f4b45afa74e9d54ebb2c62435c
Ayodele <fortunatefikayomi@gmail.com>
2022-09-14 09:21:39 -0700

    delete

---
+++ 0x01-variables_if_else_while/fikayomi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+int c = '0'
+if (c <= '19' ; c++)
+{
+while (c = '0')
+putchar(c);
+}
+if ( c != '20')
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
