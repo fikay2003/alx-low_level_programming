ebe39565e219a060f01787a9f7c704fe380236b5
Ayodele <fortunatefikayomi@gmail.com>
2022-09-14 19:37:15 -0700

    smaller

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+* main - to check if c is lowercase
+*
+* Return: always 0
+*/
+int _islower(int c)
+{
+c = 'H';
+if (_islower)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+c = 'o';
+if (_islower)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+c = 108;
+if (_islower)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
