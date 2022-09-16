55c473974c72dd5792c1e3f3d42ebd2466d6be6e
Ayodele <fortunatefikayomi@gmail.com>
2022-09-15 18:06:32 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* -isupper - Function use to check if c is a uppercase lette
+*
+* Return: Always 0
+*/
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+else
+return (0);
+}
