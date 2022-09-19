a29c6d14b55d2db1f1c507fa58dcaa06221bfb1d
Ayodele <fortunatefikayomi@gmail.com>
2022-09-19 12:54:42 -0700

    length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
-* int _strlen - is the function that returns the length of a string
-* @*s: takes a string input
+* _strlen - is the function that returns the length of a string
+* @*s: char input
 * Return: always 0
 */
 int _strlen(char *s)
 {
-strlen(*s)
+char s1[12] = "My first strlen!";
+int  len;
+len = strlen(s1);
 }
