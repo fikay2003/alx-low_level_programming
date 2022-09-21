613b2f7ab826b9020efe141fc4e5bcca1a0e9fb5
Ayodele <fortunatefikayomi@gmail.com>
2022-09-20 19:33:03 -0700

    converting string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *_atoi - convert string to an integer.
-*@s: Pointer to a character string..
+*@s: Pointer to a character string.
 *
 *Return: void
 */
@@ -13,8 +13,9 @@ char *temp;
 temp = s;
 num = 0;
 sign = 1;
-while (*temp == '-')
+while (*temp != '\0' && (*temp < '\0' || *temp > '9'))
 {
+if (*temp == '-')
 sign *= -1;
 temp++;
 }
