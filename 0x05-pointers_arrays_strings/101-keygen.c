69bd6555878433f0f7509e251c173a0d769e54ac
Ayodele <fortunatefikayomi@gmail.com>
2022-09-20 19:40:15 -0700

    random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - Entry point
+ * main - generates random vaild passwords
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-
-	return (0);
+int pass, sum:
+srand(time(NULL);
+sum = 0;
+while (sum <= 2645)
+{
+pass = (rand () % 128);
+sum += pss;
+printf("%c", pass);
+}
+printf("%c", 2772 - sum);
+return (0);
 }
