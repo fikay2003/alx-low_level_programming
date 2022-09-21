9ac4ef43f1cfc872e02cd7f520e09120de9e678b
Ayodele <fortunatefikayomi@gmail.com>
2022-09-20 19:03:04 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#inlclude "main.h"
+/**
+*
+*
+*
+*/
+void print_array(int *a, int n)
+{
+int i;
+for (o =  0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i != n - 1)
+{
+printf(",");
+printf(" ");
+}
+}
