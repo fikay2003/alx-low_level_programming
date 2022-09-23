16c6ec5e8ae7208c7ba0bf426c00754f82915628
Ayodele <fortunatefikayomi@gmail.com>
2022-09-22 20:31:41 -0700

    concatenates

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include "main.h"
+iinclude "main.h"
 /**
 * strcat - concatenates the strings pointed to by @src, including the termiant
 *		null byte, to the end of the dtring pointed to by @dest.
@@ -12,7 +12,7 @@ char *_strcat(char *dest, char *src)
 int index = 0, dest_len = 0;
 while (dest[index++])
 dest_len++;
-for (index =0; src[index]; index++)
+for (index = 0; src[index]; index++)
 dest[dest_len++] = src[index];
 return (dest);
 }
