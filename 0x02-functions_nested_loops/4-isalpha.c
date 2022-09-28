277b0860de6390b6f9178772c65b35909e46f98e
Ayodele <fortunatefikayomi@gmail.com>
2022-09-27 19:44:38 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
 * @c: is the int that will be used for argument in the function
 * Return: 0
 */
-int  function that checks for alphabetic character_isalpha(int c)
+int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
