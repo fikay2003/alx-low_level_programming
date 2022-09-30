018311b0978ebaf78b6a4d75c1c2caae6bf733c8
Ayodele <Fortnatefikayomi@gmail.com>
2022-09-30 14:03:43 -0700

    ars

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * @argc: argument count
+ * @argv: is a vector of C string
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+		return (0);
+}
