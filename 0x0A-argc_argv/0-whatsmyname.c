9e25c935d0a374324bb3fdc28d8e7646430a42ac
Ayodele <Fortnatefikayomi@gmail.com>
2022-09-30 13:33:29 -0700

    primt

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* void- entry point
+* @argc: it is an argument count
+* @argv: is a vector of c string
+*
+* Return: nothing
+*/
+void main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s ", argv[i]);
+	}
+	printf("\n");
+}
