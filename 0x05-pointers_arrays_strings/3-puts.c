5453a4a6ccebbf4aa45a89b52d07ec106ff8f9ff
Ayodele <Fortnatefikayomi@gmail.com>
2022-10-02 03:07:10 -0700

    message

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main- entry point
+ * _puts: function used to print string
+ * @str: pointer to string
+ * return: always 0
+ */
+void _puts(char *str);
+char str[20];
+_putchar(str);
+_putchar("\n");
+return (0);
+}
