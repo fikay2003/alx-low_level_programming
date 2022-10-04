0f7a26c978dc11b184c8e83ac58b54499fb1a102
Ayodele <Fortnatefikayomi@gmail.com>
2022-10-03 17:00:53 -0700

    2-argc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n" argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
