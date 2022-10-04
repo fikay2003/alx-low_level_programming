69ed83b12add1c4b5bf29604359a415017d6c3d4
Ayodele <Fortnatefikayomi@gmail.com>
2022-10-03 17:03:57 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,14 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * main- print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
  *
- *
- *
- *
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
+
 	int count = 0;
+
 	if (argc > 0)
 	{
 		while (count < argc)
