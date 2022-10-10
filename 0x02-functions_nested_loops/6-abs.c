748ee97aadc248dc99f60450bc371a10c95d5223
Ayodele <Fortnatefikayomi@gmail.com>
2022-10-10 05:36:08 -0700

    dog

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main- entry point
+ * @_abs:function to find the  absolute value of an integer
+ *return: always 0
+ */
+int _abs(int)
+{
+	int i = (x >> 31);
+	return (x ^ y) - y;
+}
---
+++ dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+}
