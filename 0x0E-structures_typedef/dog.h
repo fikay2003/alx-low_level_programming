2147863ab0e30ef65d8100cf6410304af0f0d2c6
Ayodele <Fortnatefikayomi@gmail.com>
2022-10-10 06:43:21 -0700

    hederfile

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
+/**
+ * struct dog - to group the variables of dog into one place.
+ * @name: the value of the variable char
+ * @age: the value of the variable float
+ * @owner: the value of the variable char
+ */
 struct dog
 {
-char* name;
-float age;
-char* owner;
+char *name;
+float *age;
+char *owner;
 };
-#endif /*DOG_H*/
+#endiif /*DOG_H*/
