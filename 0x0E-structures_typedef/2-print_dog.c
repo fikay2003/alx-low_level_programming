763e10bfd6e292783617db8683bcd0b5d92bfeef
Ayodele <Fortunatefikayomi@gmail.com>
2022-10-10 18:38:51 -0700

    dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,12 +14,12 @@ if (d->name == NULL)
 printf("Name: (nil)");
 else
 printf("Name: %s\n", d->name);
-if(d->age<0)
+if (d->age < 0)
 printf("Age:(nil)\n");
 else
-printf("Age:%f\n",d->age);
-if(d->owner == NULL)
+printf("Age:%f\n", d->age);
+if (d->owner == NULL)
 printf("Owner:(nil)\n");
 else
-printf("Owner:%s\n",d->owner);
+printf("Owner:%s\n", d->owner);
 }
