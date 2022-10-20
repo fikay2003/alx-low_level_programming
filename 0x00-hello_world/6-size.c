3258879fab956539323a599bdc0dff96dd32a27d
Ayodele <Fortunatefikayomi@gmail.com>
2022-10-20 10:01:53 +0100

    size of datatypes in 64bits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  *
- * return is always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -10,12 +10,10 @@ int main(void)
 	float floatType;
 	char charType;
 	double doubleType;
-	/*
-	 * size of evaluates the size of variables
-	 */
-	printf("size of int: %zu bytes\n", sizeof(intType));
-	printf("size of float: %zu bytes\n", sizeof(floatType));
-	printf("size  of char: %zu bytes\n", sizeof(charType));
-	printf("size of double: %zu bytes\n", sizeof(doubleType));
+
+	printf("size of int: %lu bytes\n", sizeof(intType));
+	printf("size of float: %lu bytes\n", sizeof(floatType));
+	printf("size  of char: %lu bytes\n", sizeof(charType));
+	printf("size of double: %lu bytes\n", sizeof(doubleType));
 	return (0);
 }
