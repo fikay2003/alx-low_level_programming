38769641b76710ee34f9034501245fa5ed716f8b
Ayodele <Fortunatefikayomi@gmail.com>
2022-10-20 10:14:04 +0100

    size of datatypes in 64bits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	int intType;
-	float floatType;
 	char charType;
+	int intType;
 	long int longIntType;
 	long long int longLongIntType;
+	float floatType;
 
-	printf("size of a int: %lu bytes\n", sizeof(intType));
-	printf("size of a float: %lu bytes\n", sizeof(floatType));
 	printf("size  of a char: %lu bytes\n", sizeof(charType));
+	printf("size of a int: %lu bytes\n", sizeof(intType));
 	printf("size of a long int: %lu bytes\n", sizeof(longIntType));
 	printf("size of a long long int: %lu bytes\n", sizeof(longLongIntType));
+	printf("size of a float: %lu bytes\n", sizeof(floatType));
 	return (0);
 }
